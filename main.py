6c7a96db1cd813abff9d394d396d53f6add21794
shiba <toshiki10490720@gmail.com>
2023-12-28 09:39:08 +0900

    upl

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import pandas as pd
 import joblib
 import numpy as np
 
+
 def load_model(model_path):
     """モデルを読み込む関数"""
     return joblib.load(model_path)
 
-def predict(input_data, scaler, models):
-    """予測を行う関数"""
-    # 入力データの標準化
-    scaled_data = scaler.transform(np.array([input_data]))
 
-    # モデルによる予測
-    predictions = {}
-    for model_name, model in models.items():
-        predictions[model_name] = model.predict(scaled_data)[0]
-    return predictions
+def predict(input_data, scaler, models):
+    try:
+        # 入力データが1次元の場合、2次元に変換
+        if input_data.ndim == 1:
+            input_data = input_data.reshape(1, -1)
+
+        # 入力データの標準化
+        scaled_data = scaler.transform(input_data)
+
+        # モデルによる予測
+        predictions = {}
+        for model_name, model in models.items():
+            if model is not None:  # モデルがNoneでないことを確認
+                predictions[model_name] = model.predict(scaled_data)[0]
+            else:
+                predictions[model_name] = None
+        return predictions
+    except Exception as e:
+        st.error(f"予測中にエラーが発生しました: {e}")
+        return None
+
 
 def main():
     # タイトル
@@ -31,16 +44,19 @@ def main():
     }
 
     # ユーザー入力
-    input_value = st.number_input("入力値 (Distal diameter)", value=0.0)
+    input_value = st.number_input("入力値 (Distal diameter)", value=5.0)
+    ## numpy配列に変換
+    input_value = np.array([[input_value]])
 
     # 予測実行ボタン
     if st.button('予測実行'):
         # 予測
         predictions = predict(input_value, scaler, models)
 
+        st.markdown(f"## AIによるFDコイル予測結果")
         # 予測結果の表示
-        st.write(f"RFモデルによる予測: {predictions['RF']}")
-        st.write(f"LGBMモデルによる予測: {predictions['LGBM']}")
+        st.write(f"RFモデルによる予測: {predictions['RF']:.03}")
+        st.write(f"LGBMモデルによる予測: {predictions['LGBM']:.03}")
 
 if __name__ == '__main__':
     main()
