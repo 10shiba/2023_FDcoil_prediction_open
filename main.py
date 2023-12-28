67be9e5f085ade679fba4bde590d19be5c13c565
shiba <toshiki10490720@gmail.com>
2023-12-28 09:56:56 +0900

    mod bag

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import numpy as np
 
 def load_model(model_path):
     """モデルを読み込む関数"""
-    st.write(f"モデルを読み込み中: {model_path}")
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
+    return model
 
 
 def predict(input_data, scaler, models):
